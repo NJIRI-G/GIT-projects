97465348eb1086f4c936fcb206695d240919cf7e
NJIRI-G <nelsongathogo123@gmail.com>
2024-11-14 14:08:54 +0300

    week2python

--- user_input.py
+++ user_input.py
@@ -1,5 +1,14 @@
-name = input("What is your name?")
-age = input("What is your age?")
-location = input("Where do you live?")
+# name = input("What is your name?")
+# age = input("What is your age?")
+# location = input("Where do you live?")
 
-print("How are you"  + name, "You are"  + age, "years old and live in" + location)
+# print("How are you"  + name, "You are"  + age, "years old and live in" + location)
+
+#Assignment
+num1 = int(input("What is your first number?"))
+num2 = int(input("What is your second number"))
+
+sum = num1 + num2
+
+print(sum)
+
---
+++ week 2.py
@@ -0,0 +1,11 @@
+my_list = [10,20,30,40]
+
+my_list.insert(1,15)
+
+myList = [50,60,70]
+
+my_list.extend(myList)
+
+my_list.sort()
+
+print(my_list.index(30))
