83d0c93b6162d91919cb0a53dba0f668a2775a51
NJIRI-G <nelsongathogo123@gmail.com>
2024-12-17 16:02:04 +0300

    Project

---
+++ projects.py/adventure.py
@@ -0,0 +1,11 @@
+name = input("Type your name: ")   #9 hours with TECH WITH TIM
+print("Welcome",name,"to this adventure")
+
+answer = input("You are on a dirt road,it has come to an end you can go left or right which way will you go ?").lower()
+
+if answer == "Left":
+    print()
+elif answer == "Right":
+    print()
+else:
+    print()
