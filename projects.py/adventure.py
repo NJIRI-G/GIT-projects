2d2c0d42013fd5f573094761e07284881c863088
NJIRI-G <nelsongathogo123@gmail.com>
2025-01-24 11:58:16 +0300

    API build 1

--- projects.py/adventure.py
+++ projects.py/adventure.py
@@ -1,12 +1,47 @@
-name = input("Type your name: ")   #9 hours with TECH WITH TIM
-print("Welcome",name,"to this adventure")
+# name = input("Type your name: ")   #9 hours with TECH WITH TIM
+# print("Welcome",name,"to this adventure")
 
-answer = input("You are on a dirt road,it has come to an end you can go left or right which way will you go ?").lower()
+# answer = input("You are on a dirt road,it has come to an end you can go left or right which way will you go ?").lower()
 
-if answer == "Left":
-     print()
-elif answer == "Right":
-     print()
-else:
-     print()
+# if answer == "Left":
+#      print()
+# elif answer == "Right":
+#      print()
+# else:
+#      print()
 
+# x = 1
+# x = x == x
+
+# print(x)
+ 
+# i = 0
+# while i <= 3 :
+#     i += 2
+#     print("*")
+
+
+# i = 0
+# while i <= 5 :
+#     i += 1
+#     if i % 2 == 0:
+#       break
+#     print("*")
+ 
+
+# for i in range(1):
+#     print("#")
+# else:
+#     print("#")
+ 
+# var = 0
+# while var < 6:
+#     var += 1
+#     if var % 2 == 0:
+#         continue
+#     print("#")
+
+var = 1
+while var < 10:
+    print("#")
+    var = var << 1
